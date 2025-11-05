6e0f8eacac1d98e0e1c5c2bc66864f5d16c7fe78
ayikubenjamin <ayikutetteh70@gmail.com>
2025-11-05 23:23:51 +0000

    completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,13 @@ year.innerHTML = `©${theDate.getFullYear()}, Benjamin Ayiku, Accra Ghana`
 
 const lastModified = document.querySelector('#lastmodified')
 lastModified.innerHTML = `Last Modified : ${theDate.toLocaleString("en-Us")}`
+
+
+
+const hamburger = document.querySelector('#nav-button');
+const nav = document.querySelector('.nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
